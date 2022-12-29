606a5854555d93f9c6f90f73d6e8ea6a56d04432
ipakey <bornagainyfke@gmail.com>
2022-12-29 12:17:29 +0000

    update

--- assests/workings/scripts.js
+++ assests/workings/scripts.js
@@ -1,2 +1,20 @@
 const date = new Date();
-console.log(date);
+const month = date.getMonth();
+
+
+const months = [
+    "January",
+    "Febrary",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+];
+
+document.querySelector(".date h4").innerHTML =  months[5];
